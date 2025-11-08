f5e6c8b67ae2d6268fe971c65dfbe5d2f3dfe35e
mieszko4 <mieszko4@gmail.com>
2025-11-08 13:58:16 +0100

    feat: clean up Custom Video View

--- App.tsx
+++ App.tsx
@@ -10,8 +10,7 @@ import {
   Platform,
 } from 'react-native';
 
-import ZoomUs, {ZoomEmitter, ZoomUsVideoView} from 'react-native-zoom-us';
-import {type NativeLayoutUnit} from 'react-native-zoom-us/native';
+import ZoomUs, {ZoomEmitter} from 'react-native-zoom-us';
 
 import {extractDataFromJoinLink} from './api/extractDataFromJoinLink';
 
@@ -43,92 +42,12 @@ const exampleJoinLink = 'https://us02web.zoom.us/j/MEETING_NUMBER?pwd=PASSWORD';
 
 const exampleJoinMeeting = extractDataFromJoinLink(exampleJoinLink);
 
-type CustomViewerProps = {
-  leaveMeeting: () => void;
-};
-
-const CustomViewer = ({leaveMeeting}: CustomViewerProps) => {
-  const [showScreenShare, setShowScreenShare] = useState(false);
-
-  const activeConfig: NativeLayoutUnit = {
-    kind: 'active',
-    x: 0,
-    y: 0,
-    width: 1,
-    height: 1,
-  };
-
-  const activeShareConfig: NativeLayoutUnit = {
-    kind: 'active-share',
-    x: 0,
-    y: 0,
-    width: 1,
-    height: 1,
-  };
-
-  return (
-    <View style={StyleSheet.absoluteFillObject}>
-      <Button onPress={leaveMeeting} title="Leave meeting" />
-      <Button onPress={() => ZoomUs.startShareScreen()} title="Share screen" />
-      <Button
-        onPress={() => setShowScreenShare(!showScreenShare)}
-        title={!showScreenShare ? 'Show screen' : 'Show video'}
-      />
-      <ZoomUsVideoView
-        style={styles.customViewer}
-        layout={[
-          // TODO: check why showing both at the same time does not work
-          showScreenShare ? activeShareConfig : activeConfig,
-          // Selfcamera preview
-          {
-            kind: 'preview',
-            // The percent of video view (required)
-            x: 0.73,
-            y: 0.73,
-            width: 0.25,
-            height: 0.2,
-            // Enable border (optional)
-            border: true,
-            // Disable show user name (optional)
-            showUsername: false,
-            // Show audio off (optional)
-            showAudioOff: true,
-            // Background color (optional)
-            background: '#ccc',
-          },
-          /*
-          // share video
-          {
-            kind: 'share',
-            // The index of user list (required)
-            userIndex: 0,
-          },
-          // Specify attendee
-          {
-            kind: 'attendee',
-            // The index of user list (required)
-            userIndex: 0,
-          },
-          {
-            kind: 'attendee',
-            userIndex: 1,
-          },
-          */
-        ]}
-      />
-    </View>
-  );
-};
-
-// 3. `TODO` Enable custom view (android only)
-const enableCustomizedMeetingUI = false;
-
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [isInitialized, setIsInitialized] = useState(false);
   const [isMeetingOngoing, setIsMeetingOngoing] = useState(false);
 
-  console.log({isDarkMode});
+  console.log({isDarkMode, isMeetingOngoing});
 
   useEffect(() => {
     (async () => {
@@ -137,7 +56,6 @@ const App = () => {
           {jwtToken: sdkJwtToken},
           {
             language: 'pt-PT',
-            enableCustomizedMeetingUI,
           },
         );
 
@@ -210,17 +128,6 @@ const App = () => {
     }
   };
 
-  const leaveMeeting = async () => {
-    try {
-      const leaveMeetingResult = await ZoomUs.leaveMeeting();
-
-      console.log({leaveMeetingResult});
-    } catch (e) {
-      Alert.alert('Error', 'Could not execute leaveMeeting');
-      console.error('ERR', e);
-    }
-  };
-
   return (
     <>
       <View style={styles.container}>
@@ -236,9 +143,6 @@ const App = () => {
           disabled={!isInitialized}
         />
       </View>
-      {enableCustomizedMeetingUI && isMeetingOngoing && (
-        <CustomViewer leaveMeeting={leaveMeeting} />
-      )}
     </>
   );
 };
@@ -249,10 +153,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  customViewer: {
-    width: '100%',
-    flex: 1,
   },
 });
 
